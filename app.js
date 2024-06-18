b8b03041efa406a5bfc6b41e3dcbdca249a59648
rosadavi <davi.rosa.ofc@gmail.com>
2024-06-18 12:30:40 -0300

    adicionando nodemon no projeto, e configurando rotas a partir de outro diretorio

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3333;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+import usuario from './router/usuario.js';
+app.use('/', usuario);
 
 app.listen(port, () => {
     console.log(`Servidor escutando na porta: http://localhost:${port}`);
