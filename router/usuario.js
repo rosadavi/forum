36b0d48570123bb411cf6aaf8b899dc2cc2aebfa
rosadavi <davi.rosa.ofc@gmail.com>
2024-06-18 15:44:43 -0300

    adicionando rotas de criacao de usuario

--- router/usuario.js
+++ router/usuario.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import Usuario from '../models/Usuario.js'
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -11,6 +12,19 @@ router.get('/login', (req, res) => {
 
 router.get('/registrar', (req, res) => {
     res.render('public/registrar.handlebars');
+});
+
+router.post('/registrar', (req, res) => {
+    const {nome, sobrenome, email, senha} = req.body;
+
+    Usuario.create({
+        nome,
+        sobrenome,
+        email,
+        senha,
+    });
+
+    res.send('Usuario cadastrado com sucesso.');
 });
 
 export default router;
